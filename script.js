ea865888dd8eade7d4437c0fc8e1c24a79e94053
Alicia BELCHIOR <alicia.belchior@outlook.com>
2024-09-11 16:55:49 +0200

    SITE V4

---
+++ script.js
@@ -0,0 +1,11 @@
+//identifier sur le bouton 
+const btn = document.getElementById("btn")
+
+//definir une function 
+function handleClick(e){
+    e.target.innerhtml = "Bonjour"
+    window.location ="https://www.google.com/"
+}
+
+//rajouter un click event 
+btn.addEventListener("click", handleClick)
