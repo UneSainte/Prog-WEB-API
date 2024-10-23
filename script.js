627470b28d170e2fc7dee1092b45560e2af8e24e
Alicia BELCHIOR <alicia.belchior@outlook.com>
2024-10-23 16:55:28 +0200

    SITE V5

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //identifier sur le bouton 
-const btn = document.getElementById("btn")
+const btn = document.getElementById("btn1")
 
 //definir une function 
 function handleClick(e){
-    e.target.innerhtml = "Bonjour"
-    window.location ="https://www.google.com/"
+    e.target.innerHTML = "Alors ? Convaincu ?"
+    window.open("https://store.steampowered.com/app/252330/Slender_The_Arrival/", "_blank")
 }
 
 //rajouter un click event 
