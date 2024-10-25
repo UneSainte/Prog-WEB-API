3d6395b57d31fccd74821d165467342808776bed
Alicia BELCHIOR <alicia.belchior@outlook.com>
2024-10-25 15:22:10 +0200

    Add javascript.js

---
+++ js/javascript.js
@@ -0,0 +1,15 @@
+let student = {nom : "Marie", age : 20, courses : [] };
+
+student.age = 21;
+student.grade = "A";
+
+student.courses.push("Math", "Physics","Chemistry");
+
+let index = student.courses.indexOf("Physics");
+let slice = student.courses.slice(0, 2);
+
+
+console.log(student);
+console.log(index);
+console.log(slice);
+
